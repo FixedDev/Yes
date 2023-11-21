383dfa1166d50303db8073029f08f4e69f28fa10
Damian G <gilbertodamian14@gmail.com>
2023-11-21 00:48:23 -0600

    docs: Add docs for TrollType#id and TrollType#displayName

--- src/main/java/me/fixeddev/troll/troll/TrollType.java
+++ src/main/java/me/fixeddev/troll/troll/TrollType.java
@@ -4,8 +4,18 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
 public interface TrollType {
+    /**
+     * The id of this TrollType.
+     *
+     * @return The id of this TrollType.
+     */
     String id();
 
+    /**
+     * The name to display in all messages of this TrollType.
+     *
+     * @return A {@linkplain Component} representing the display name of this TrollType.
+     */
     Component displayName();
 
     void executeTroll(Player troll, Player trolled);
