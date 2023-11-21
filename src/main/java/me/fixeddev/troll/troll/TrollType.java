90a0d96d98441d903170309ccea7b9db9071580c
Damian G <gilbertodamian14@gmail.com>
2023-11-21 14:08:01 -0600

    feat: Add TrollType#executeTroll using the User type. It automatically adds the troll into the history of the trolled player.

--- src/main/java/me/fixeddev/troll/troll/TrollType.java
+++ src/main/java/me/fixeddev/troll/troll/TrollType.java
@@ -1,7 +1,11 @@
 package me.fixeddev.troll.troll;
 
+import me.fixeddev.troll.user.User;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.Map;
 
 public interface TrollType {
     /**
@@ -17,6 +21,11 @@ public interface TrollType {
      * @return A {@linkplain Component} representing the display name of this TrollType.
      */
     Component displayName();
+
+    default void executeTroll(User troll, User trolled) {
+        executeTroll(Bukkit.getPlayer(troll.id()), Bukkit.getPlayer(trolled.id()));
+        trolled.trollHistory().add(Map.entry(troll.id(), this));
+    }
 
     void executeTroll(Player troll, Player trolled);
 }
